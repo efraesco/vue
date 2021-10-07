107ccbdd4ec87a0044ced1107ea835af3cf7fff5
efraesco <efraesco@gmail.com>
2021-10-07 00:48:20 -0500

    Inicios Vue

---
+++ js/introvue.js
@@ -0,0 +1,20 @@
+let contador = 0;
+setInterval(() => {
+    const spanNumero = document.getElementById("numero");
+    spanNumero.innerHTML = contador++;
+}, 1000);
+
+
+const Counter = {
+    data() {
+        return {
+            counter: 0,
+            message: "Contador Vue"
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.counter++;
+        }, 1000);
+    }
+}
